a55649ed3b9c8ad844e366a729a45d976570ab8b
ezequiel <ezeedge@gmail.com>
2021-01-05 23:59:48 -0300

    mostrando posttype

--- src/components/PortafolioItems.js
+++ src/components/PortafolioItems.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import styled from 'styled-components'
 import {graphql, grapqhl , StaticQuery,Link} from 'gatsby'
+
+
+const PortfolioItemsWrapper = styled.div`
+
+  display: flex;
+  justify-content: center;
+
+`
+
+const PortfolioItem = styled.div`
+
+width: 300px;
+border: 1px solid #efefef;
+margin: 16px;
+padding: 16px;
+
+`
+
+const PortfolioImg = styled.img`
+
+max-width: 100%;
+
+`
 
 const PortafolioItems = () => {
     return ( 
@@ -24,17 +48,21 @@ const PortafolioItems = () => {
 }
         
         `}
-    render={props => props.allWordpressWpPortfolio.edges.map(portafolioItem => (
-        <div key={portafolioItem.node.id}>
+    render={props => (<PortfolioItemsWrapper> {props.allWordpressWpPortfolio.edges.map(portafolioItem => (
+        <PortfolioItem key={portafolioItem.node.id}>
 
             <h2>{portafolioItem.node.title}</h2>
-            <img src={portafolioItem.node.featured_media.source_url} alt="thumbnail" />
+            <PortfolioImg src={portafolioItem.node.featured_media.source_url} alt="thumbnail" />
             <div  dangerouslySetInnerHTML={{__html : portafolioItem.node.excerpt}} ></div>
         <Link to={`/portfolio/${portafolioItem.node.slug}`}>Leer mas</Link>
-        </div>
+        </PortfolioItem>
     ))}
+    </PortfolioItemsWrapper>
+    )}
+    
     
         />
+        
      );
 }
  
